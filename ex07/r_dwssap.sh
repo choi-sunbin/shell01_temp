414e22ed9cb234a095bf948df0ca4c44262231e7
Sunbin Choi <sunbchoi@c7r2s4.42seoul.kr>
2020-11-20 01:12:06 +0900

    ex07

---
+++ ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | sed '/'#'/d' | awk 'NR %2 == 0 { print }'cat /etc/passwd | sed '/'#'/d' | awk 'NR %2 == 0 { print }' | sed 's/:.*//' | rev | sort -r | sed -n ${FT_LINE1},${FT_LINE2}p | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/./g' 
